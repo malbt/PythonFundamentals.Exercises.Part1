2fc19acc05fdbc994b17114142cf2a53474a814a
Maleda Tessema <mtessema@zipcodes-MacBook-Pro-3.local>
2020-03-03 19:31:58 -0500

    Exercise 6

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,3 @@
+fahrenheit = input("Temperature in fahrenheit:")                                                                          
+celsius =  (int(fahrenheit)-32) * 5/9                                                          
+print(fahrenheit + " degrees fahrenheit is equal to "+ str(celsius) +" degree(s) celsius")     
